b608542457ab5aa1e43b1905be086a22eb618018
Olti-s <sogojevaolti451@gmail.com>
2025-02-18 17:47:42 +0100

    ........

--- functions.php
+++ functions.php
@@ -1,11 +1,14 @@
 <?php 
 
-function add_theme_scripts(){
- wp_enqueue_style("style",get_template_directory_uri()."/style.css",false,'1.0',"all");
- wp_enqueue_scripts("main",get_template_directory_uri()."/js/main.js",array('jquery'),"1.0",  true);
-
+function add_theme_script(){
+ wp_enqueue_style("style",get_template_directory_uri()."/style.css",false,"1.0","all");
+ wp_enqueue_script("main",get_template_directory_uri()."/js/main.js",array('jquery'),"1.0",  true);
+  
+ if(is_singular() && comments_open() && get_option('thread-comments') ){
+    wp_enqueue_script('comment-replay');
+ }
 }
 
               
-add_action("wp_enqueue_scripts","add_theme_scripts");
+add_action("wp_enqueue_scripts","add_theme_script");
 ?>
