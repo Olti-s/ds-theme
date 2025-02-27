b2d1bbfd21205c7b27508168497383cf77158e68
Olti-s <sogojevaolti451@gmail.com>
2025-02-27 17:42:26 +0100

    ora 4

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 function add_theme_script(){
  wp_enqueue_style("style",get_template_directory_uri()."/style.css",false,"1.0","all");
+ wp_enqueue_style("main",get_template_directory_uri()."/css/main.css",false,"1.0","all");
  wp_enqueue_script("main",get_template_directory_uri()."/js/main.js",array('jquery'),"1.0",  true);
   
  if(is_singular() && comments_open() && get_option('thread-comments') ){
--- page-about-me.php
+++ page-about-me.php
@@ -4,16 +4,24 @@
 
 <?php  get_header();     ?>
 
+<div class="container">
+  <div class="first">
+    <h1>About page posts</h1>
+      <?php
+        if(have_posts()):
+        while(have_posts()): the_post();?>
+      <h2><?php   the_title();   ?></h2>
+      <p><?php the_content();   ?></p>
+      <?php endwhile;   ?>
+      <?php endif;?>
+  </div>
+  <div class="second">
+          <h1>Sidebar</h1>
+  </div>
 
-<h1>hello</h1>
-<?php
-  if(have_posts()):
-  while(have_posts()): the_post();?>
-<h2><?php   the_title();   ?></h2>
-<p><?php the_content();   ?></p>
-<?php endwhile;   ?>
-<?php endif;?>
  
+</div>
+
 
 
 <?php  get_footer()  ;   ?>
